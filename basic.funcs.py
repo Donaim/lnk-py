098ff8342a58ad191f5201a25863830c4c6ab794
Donaim <0@kek.com>
2018-03-04 22:03:52 +0100

    fix local run args, replaced pyincluder

--- basic.funcs.py
+++ basic.funcs.py
@@ -19,7 +19,7 @@ def local(at):
     if isdir: path += 'lnkpy-run.py'
     
     try:
-        subprocess.check_call([path] + sys.argv, shell=True)
+        subprocess.check_call([path] + sys.argv[1:], shell=True)
         # subprocess.Popen([path] + sys.argv[1:], shell=True, stdin=None, stdout=None, stderr=None, close_fds=False)
     except Exception as ex:
         print("Couldn't open file {}".format(path), file=sys.stderr)
