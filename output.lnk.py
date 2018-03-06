e8190f8c83fef3149989383c4f09a718f1857085
Donaim <0@kek.com>
2018-03-06 09:45:37 +0100

    improved tag splitting

--- output.lnk.py
+++ output.lnk.py
@@ -1,17 +1,20 @@
-TARGET_INFO='''
-~/Desktop/Probf/primitive.py
-https://github.com/Donaim/ProblemFlawiusza.git
-https://raw.githubusercontent.com/Donaim/ProblemFlawiusza/master/primitive.py
-'''
-
-# wyzej miejsce dla adresow. wyszukiwanie jest pryorytetowane z gory do dolu
-# second non-emty non-comment line is defined to be the beginning of TARGET_INFO string
-
 import re
 import os
 import sys
 import subprocess
 import urllib.request
+
+
+
+TARGET_INFO='''
+~/Desktop/Probf/primitive.py
+https://github.com/Donaim/ProblemFlawiusza.git
+# https://raw.githubusercontent.com/Donaim/ProblemFlawiusza/master/primitive.py
+'''
+
+# wyzej miejsce dla adresow. wyszukiwanie jest pryorytetowane z gory do dolu
+# second non-emty non-comment line is defined to be the beginning of TARGET_INFO string
+
 
         ########
        ## TAGS ##
@@ -250,9 +253,9 @@ filtered  = filter(lambda line: len(line) > 0 and not line.isspace() and line[0]
 # for stderr
 
 class arg(object):
-    def __init__(self, group_tag):
+    def __init__(self):
         self.command = None
-        self.tags = [group_tag]
+        self.tags = []
         self.tags_dict = tags_dict
         self.args = args
     def invoke_tags(self):
@@ -273,36 +276,45 @@ class tag(object):
             return False
     def by_name(name):
         if name in tags_dict: return tags_dict[name]
-        else: raise Exception("unknown tag: {}".format(name))
+        else: raise Exception("unknown tag: \"{}\"".format(name))
         # else: return curr
 
 tags_dict = dict(map(lambda p: (p[0], tag(p[0], p[1])), tag_funcs_di.items()))
 args = []
 
-def is_tag(line): return line[0] == '$'
-def is_group_tag(line): is_tag(line) and line[-1] == '$'
-def next_group_tag(line):
-    tname = line[1:-1]
-    return tag.by_name(tname)
-
-def parse_tags(a: arg, line, curr):
-    sp = line[1:].split('$')
-    a.command = "$".join(sp[1:])
-    for tname in sp[0].split():
-        a.tags.append(tag.by_name(tname))
+def is_tag(line: str) -> bool: return line.lstrip()[0] == '$'
+def is_group_tag(first: str, second: str) -> bool: return len(second) == 0
+def parse_tags(first: str) -> list:
+    if len(first) == 0: return []
+    return list(map(lambda tname: tag.by_name(tname), first.split(' \t,')))
+def find_tag_close(line: str) -> int:
+    for (i, c) in enumerate(line):
+        if c == '$': return i
+    return 0
+def split_by_tag(line: str) -> (str, str):
+    if is_tag(line):
+        line = line.lstrip()[1:] # skip first '$' char
+        close_index = find_tag_close(line)
+        first = line[:close_index].strip(' \t,')
+        second = line[close_index:].lstrip()
+        return (first, second)
+    else:
+        return ('', line)
 
 def parse_args(lines):
-    curr = tags_dict[DEFAULT_TAG]
+    group_tags = [tags_dict[DEFAULT_TAG]]
     for line in lines:
-        if is_group_tag(line):
-            curr = next_group_tag(line)
+        (first, second) = split_by_tag(line)
+        if is_group_tag(first, second):
+            group_tags = parse_tags(first)
         else:
-            a = arg(curr)
-            if is_tag(line): parse_tags(a, line, curr)
-            else: a.command = line
+            a = arg()
+            a.tags = group_tags + parse_tags(first)
+            a.command = second
             args.append(a)
 
 parse_args(filtered)
 
+for a in args: print('arg \"{}\" has tags={}'.format(a.command, list(map(lambda t: t.name, a.tags))))
 for a in args:
     if a.invoke_tags(): break
